7cdc196147e9299fcb4b50daafd221a00bbb207f
Rachidi Aminou <rachidiaminou@Rachidis-MacBook-Pro.local>
2019-10-22 02:03:06 -0400

    adding week1 script

---
+++ Coursera_Capstone_W1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# this notebook will be mainly used for the capstone project
+
+# In[1]:
+
+
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
